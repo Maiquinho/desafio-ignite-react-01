a7a3380f8216937662994cbce1dae0691c86b0ce
Michael Matheus <michaelmatheus2001@gmail.com>
2023-09-04 07:00:23 -0300

    style: format App tabs & add semicolons

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
       content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
       checked: false,
     },
-  ] 
+  ]; 
 
   return (
     <>
