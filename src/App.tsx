1772794ca3a1d1545cdf36bdbd1244f8ee44ea4f
Michael Matheus <michaelmatheus2001@gmail.com>
2023-09-04 10:37:02 -0300

    feat(tasks): add states, handle new task & handle new task validaty

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 import { Header } from './components/Header';
 import { PlusCircle } from '@phosphor-icons/react';
 import { EmptyTasks } from './components/EmptyTasks';
@@ -8,33 +9,63 @@ import styles from './App.module.css';
 
 export default function App() {
 
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       id: 1,
       content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-      checked: false,
+      checked: true,
     },
     {
       id: 2,
       content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-      checked: false,
+      checked: true,
     },
     {
       id: 3,
       content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
       checked: false,
     },
-  ];
+  ]); 
+
+  const [newInputTaskContent, setNewInputTaskContent] = useState('');
+
+
+  function handleAddTask(event: FormEvent){
+    event.preventDefault();
+    setTasks([...tasks, { id: tasks.length + 1, content: newInputTaskContent, checked: false }]);
+    setNewInputTaskContent('');
+  }
+
+  function handleInputTaskInvalid(event: InvalidEvent<HTMLInputElement>){
+    event.target.setCustomValidity('Digite um conteúdo válido!');
+  }
+
+  function handleInputTaskContent(event: ChangeEvent<HTMLInputElement>){
+    event.target.setCustomValidity('');
+    setNewInputTaskContent(event.target.value);
+  }
+  
 
   return (
     <>
       <Header />
 
       <main className={styles.container}>
-        <form className={styles.taskForm}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
-          <button>Criar <PlusCircle width={20} height={20} /></button>
+        <form className={styles.taskForm} onSubmit={handleAddTask}>
+          <input 
+            type="text" 
+            placeholder="Adicione uma nova tarefa" 
+            value={newInputTaskContent}
+            onChange={handleInputTaskContent} 
+            onInvalid={handleInputTaskInvalid}
+            required
+          />
+          <button>
+            Criar 
+            <PlusCircle width={20} height={20} />
+          </button>
         </form>
+
         <section className={styles.tasks}>
           <div className={styles.tasksInfo}>
             <div className={`${styles.tasksCount} ${styles.tasksCreated}`}>
@@ -52,7 +83,13 @@ export default function App() {
               <EmptyTasks />
             )}
             {tasks.length > 0 &&
-              tasks.map(task => <Task key={task.id} />)
+              tasks.map(task => (
+                <Task 
+                  content={task.content}
+                  checked={task.checked}
+                  key={task.id} 
+                />
+              ))
             }
 
           </div>
